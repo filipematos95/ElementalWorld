73ef41731e4d0bd7f98fedc7cbadc152e4d03267
Filipe <fm1995@gmail.com>
2026-02-13 12:28:28 +0100

    change pools

--- hybridmodel.py
+++ hybridmodel.py
@@ -393,7 +393,14 @@ class HybridEcosystem:
                 )
                 self.n_agents.assign_add(safe)
 
-
+        if tf.shape(active_idx)[0] > 0:
+            tf.print("DIAG -> Growth:", tf.reduce_mean(actual_growth),
+                     "Resp:", tf.reduce_mean(maint),
+                     "SpaceF:", tf.reduce_mean(space_f),
+                     "Fitness:", tf.reduce_mean(niche_fitness),
+                     "Soil_N:", tf.reduce_mean(inorg_new[:, :, 0]))
+        else:
+            tf.print("DIAG -> No active agents")
 
         return self.n_agents
 
